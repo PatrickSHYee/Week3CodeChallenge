e887f81916b2b6637ce7fdbda084b20da0ed72a6
PatrickSHYee <patrickshyee@gmail.com>
2015-02-21 23:20:55 -0700

    Programming and testing for the fastest results
    Used books and the Internet  and past assignments to find the solutions
    to these problems.  The newest thing I learned were lambda expressions
    and found it help with solving one of the problems that made recursive
    made more cents.

--- Week3CodeChallenge/Week3CodeChallenge/Program.cs
+++ Week3CodeChallenge/Week3CodeChallenge/Program.cs
@@ -8,7 +8,6 @@ namespace Week3CodeChallenge
     {
         static void Main(string[] args)
         {
-
         }
         /// <summary>
         /// A function that calculates the starting value of the longest Collatz sequence.
@@ -18,7 +17,21 @@ namespace Week3CodeChallenge
         /// <returns>Starting value of the longest sequence</returns>
         public static long LongestCollatzSequence()
         {
-            return 0; // Default return value, replace this
+            // an lambda expression to do the recursive calls
+            return Enumerable.Range(1, 999999).AsParallel().Select(x => new { num = x, count = GenSequence(x) }).OrderByDescending(x => x.count).Select(x => x.num).First();
+        }
+
+        /// <summary>
+        /// The base case is 1 for this recursive aligorthm, where the even numbers is divided by 2 and the odd numbers are times 3 plus 1
+        /// until the base case is reached. The count of the sequence is returned.
+        /// </summary>
+        /// <param name="num">a number to do the math with.</param>
+        /// <returns>1</returns>
+        private static long GenSequence(long num)
+        {
+            if (num == 1) return 1;
+            long next = (num % 2 == 0) ? num / 2 : 3 * num + 1;
+            return GenSequence(next) + 1;
         }
 
         /// <summary>
@@ -29,7 +42,23 @@ namespace Week3CodeChallenge
         /// <returns>The sum of every even number for the given number of Fibonacci digits</returns>
         public static long EvenFibonacciSequencer(long maxValue)
         {
-            return 0; // Default return value, replace this
+            // setting everything up
+            long sum_Even = 2;  // 2 is already valued, so I need this even number to be set
+            long num1 = 1;  // first number in the sequence
+            long num2 = 2;  // second number in the sequence
+            long fibNum = num1 + num2;  // the next number in the sequence
+
+            while (fibNum <= maxValue)  // checks if the fibonacci number is less than or equal to the max Value
+            {
+                if (fibNum % 2 == 0)  // checks if the fibonacci number is even
+                {
+                    sum_Even += fibNum;  // adds the even numbers together
+                }
+                num1 = num2;            // replaces the first number with the 2nd number
+                num2 = fibNum;          // replaces the 2nd number with the checked number
+                fibNum = num1 + num2;   // the next number in the sequence
+            }
+            return sum_Even;
         }
 
         /// <summary>
@@ -39,7 +68,41 @@ namespace Week3CodeChallenge
         /// <returns>The prime number which is the n'th found</returns>
         public static int FindNPrimes(int maxPrime)
         {
-            return 0; // Default return value, replace this
+            int n = 3;      // a great prime number to start with
+            int count = 2;  // this is where 3 is the count at
+
+            while (count < maxPrime)    // checks the count to the nth
+            {
+                n += 2;                 // skips the even numbers
+                // finds the number is prime
+                if (IsPrime(n))
+                {
+                    count++;
+                }
+            }
+            return n; // returns the nth prime the maxPrime
+        }
+
+        /// <summary>
+        /// Finds the prime number
+        /// </summary>
+        /// <param name="number">a test number</param>
+        /// <returns>prime returns true and not returns false</returns>
+        public static bool IsPrime(int number)
+        {
+            // I want to return a bool variable to return
+            bool thatPrime = true;
+
+            if (number < 3)
+            {
+                thatPrime = true;
+            }
+            // finding the prime number
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) thatPrime = false;
+            }
+            return thatPrime;
         }
     }
 }
